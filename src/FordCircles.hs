cb37a537da0d8b9de1190ba4aeeabc0d5d02d7b7
banditpig <banditpig@gitcommit.co.uk>
2018-06-09 18:34:24 +0100

    changed params to fordCircles

--- src/FordCircles.hs
+++ src/FordCircles.hs
@@ -6,15 +6,15 @@ import           Fractions
 
 type FordCircle = (Float, Float, Float)
 
--- for Farey of order n
-fordCircles :: (Integer -> [Fraction]) -> Integer ->  [FordCircle]
-fordCircles f = fmap fordCircle . f
+
+fordCircles :: [Fraction] ->  [FordCircle]
+fordCircles = fmap fordCircle
 
 
 
 fordCircle :: Fraction -> FordCircle
 fordCircle  (F p 0) = (0, 0, 0)
 fordCircle  (F p q) = (r, fromIntegral p / fromIntegral q, r ) where
-    r = 1/fromIntegral (2*q^2)
+    r = 1/fromIntegral (2*q*q)
 scaleFordCircle :: Float ->  FordCircle -> FordCircle
 scaleFordCircle s (x, y, z) = (s * x, s * y, s * z)
